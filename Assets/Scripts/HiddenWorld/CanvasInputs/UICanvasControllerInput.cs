fc97e72715808529be39a63413dd04159022a4ab
Tigris <matyasdudas97@gmail.com>
2023-02-08 17:35:00 -0400

    Clean 3rd person

---
+++ Assets/Scripts/HiddenWorld/CanvasInputs/UICanvasControllerInput.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace HiddenWorld
+{
+    public class UICanvasControllerInput : MonoBehaviour
+    {
+
+        [FormerlySerializedAs("starterAssetsInputs")] [Header("Output")]
+        public HiddenWorldInputs hiddenWorldInputs;
+
+        public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        {
+            hiddenWorldInputs.MoveInput(virtualMoveDirection);
+        }
+
+        public void VirtualLookInput(Vector2 virtualLookDirection)
+        {
+            hiddenWorldInputs.LookInput(virtualLookDirection);
+        }
+
+        public void VirtualJumpInput(bool virtualJumpState)
+        {
+            hiddenWorldInputs.JumpInput(virtualJumpState);
+        }
+
+        public void VirtualSprintInput(bool virtualSprintState)
+        {
+            hiddenWorldInputs.SprintInput(virtualSprintState);
+        }
+
+        public void VirtualInteractInput(bool virtualInteractState)
+        {
+            hiddenWorldInputs.InteractInput(virtualInteractState);
+        }
+        
+    }
+
+}
