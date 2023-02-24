98f0cba309fc47bd925942c5f450fdfb9b721036
DWhittley <115023269+DWhittley@users.noreply.github.com>
2023-02-24 14:59:40 -0500

    Latest puzzle scripts

--- Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -1,29 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using Helpers;
 
-public class PuzzleManager : MonoBehaviour
+namespace Puzzle
 {
-    public int puzzleID;  // If puzzles are organized by a numerical identifier
-    public Vector3 startPosition; // If puzzles "appear" we will have to establish their start position
-    public Quaternion startRotation; // Sample puzzle mechanic variable: If puzzle can be rotated then this will establish the initial view of it.  This may end up in a sub-class
-    public static bool puzzleSolved; // Flag used to denote if the puzzle object is solved
-
-    public void RotateTo(Quaternion newRotation) // sample puzzle mechanic if puzzle needs to rotate.  This may end up in a sub-class
+    public class PuzzleManager : MonoBehaviour
     {
-        transform.rotation = newRotation; 
+        public int puzzleID; // If puzzles are organized by a numerical identifier
+        public Vector3 puzzlePosition; // if puzzles "appear" we will have to estaablish their start position
+        public Quaternion puzzleRotation; // Sample puzzle mechanic variable: If puzzle can be rotated we need to set it at the start.  May be moved to puzzle subclass
+        public bool puzzleSolved; // Flag used to denote if the puzzle object is solved
+
+        public void RotateTo(Quaternion newRotation)
+        {
+            transform.rotation = newRotation;
+        }
+
+        public void StorePuzzleState() // TBD what information will be passed regarding state at savepoint
+        {
+
+        }
+
+        public void PuzzleReset() // may end up being (or calling) a puzzle sub-class dependent reset procedure
+        {
+
+        }
+
+        // May require a UI interface method for things like puzzle: progress, timer, orientation, etc.
+        // May require a puzzle completion save method
+
+        private void OnTriggerEnter(Collider other)
+        {
+            IInteractable interactable = other.GetComponent<IInteractable>();
+            if (interactable != null && other.CompareTag("Puzzle"))
+            {
+                // interactable.OnInteract(puzzle.interact); // need to determine what gets passed
+            }
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    public void StorePuzzleState()  // need to determine what inforamtion will be passed regarding puzzle states
-    {
-
-    }
-
-    public void PuzzleReset() // puzzle sub-class dependent reset procedure?
-    {
-
-    }
-
-    // Other fundamental puzzle mechanics common to all puzzle types may need to be added.
-    // May need a method that deals with UI interaction: puzzle progress, puzzle timer, etc.
 }
