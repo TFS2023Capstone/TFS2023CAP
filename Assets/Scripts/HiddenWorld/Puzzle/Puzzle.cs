b9910cbfefb1d886428427bf08e5fc9e94077e87
DWhittley <wowwit@bell.net>
2023-03-14 14:00:31 -0400

    Puzzle update
    major update with new revised mouse functions, changed to 3d position.

--- Assets/Scripts/HiddenWorld/Puzzle/Puzzle.cs
+++ Assets/Scripts/HiddenWorld/Puzzle/Puzzle.cs
@@ -5,95 +5,103 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.ProBuilder.Shapes;
 
-public class Puzzle : MonoBehaviour
+namespace HiddenWorld.Puzzle
 {
-    private bool _puzzleSolved = false; // Flag used to denote if the puzzle object is solved need get/private set
-    public bool isSequential = false; // sets whether this puzzle is a sequential puzzle
-    public List<string> list1;
-    public List<string> list2;
-    private bool _allPiecesCheck = false;
-    private bool _sequenceGood = false;
-
-    // Functions:
-
-    void ListComparer()
+    public class Puzzle : MonoBehaviour
     {
-        // List compare
-        bool equal = list1.SequenceEqual(list2);
+        private bool _puzzleSolved = false; // Flag used to denote if the puzzle object is solved need get/private set
+        public bool isSequential = false; // sets whether this puzzle is a sequential puzzle
+        public List<string> solutionList;
+        public List<string> currSolvedList;
+        [SerializeField]
+        private List<PuzzlePiece> _piecesList;
+        private bool _allPiecesCheck = false;
+        private bool _sequenceGood = false;
 
-        if (equal)
+        // Functions:
+        private void Start()
         {
-            _sequenceGood = true;
-            Debug.Log("The lists are equal.");
-        }
-        else
-        {
-            _sequenceGood = false;
-            Debug.Log("The lists are not equal.");
-        }
-    }
-
-    void ListChecker()
-    {
-        // List checker
-        bool allInList2 = list1.All(elem => list2.Contains(elem));
-
-        if (allInList2)
-        {
-            _allPiecesCheck = true;
-            Debug.Log("All elements of List 1 are in List 2.");
-        }
-        else
-        {
-            _allPiecesCheck = false;
-            Debug.Log("Not all elements of List 1 are in List 2.");
-        }
-    }
-
-    void BuildPuzzleSolution()
-    {
-        //List1 is Solution { "Piece2", "Piece1"}
-        list1 = new List<string>();
-        list1.Add("Piece2");
-        list1.Add("Piece1");
-    }
-    public void AddList2Piece(string addPiece)
-    {
-        //List2 is list built from puzzlePiece entries { "Piece1", "Piece2"}
-        list2.Add(addPiece);
-    }
-    public void ResolvePuzzle()
-    {
-
-        if (list2.Count == list1.Count)
-        {
-            if (isSequential)
+            foreach (var piece in _piecesList)
             {
-                ListComparer();
-                if (_sequenceGood)
-                {
-                    _puzzleSolved = true;
-                }
-                else
-                {
-                    _puzzleSolved = false;
-                }
+                piece.SetPuzzleRef(this);
             }
+        }
+        void ListComparer()
+        {
+            // List compare
+            bool equal = solutionList.SequenceEqual(currSolvedList);
 
-            ListChecker();
-            if (_allPiecesCheck)
+            if (equal)
             {
-                _puzzleSolved = true;
+                _sequenceGood = true;
+                Debug.Log("The lists are equal.");
             }
             else
             {
-                _puzzleSolved = false;
+                _sequenceGood = false;
+                Debug.Log("The lists are not equal.");
             }
         }
+
+        void ListChecker()
+        {
+            // List checker
+            bool allInList2 = solutionList.All(elem => currSolvedList.Contains(elem));
+
+            if (allInList2)
+            {
+                _allPiecesCheck = true;
+                Debug.Log("All elements of List 1 are in List 2.");
+            }
+            else
+            {
+                _allPiecesCheck = false;
+                Debug.Log("Not all elements of List 1 are in List 2.");
+            }
+        }
+
+        public void AddList2Piece(string addPiece)
+        {
+            //List2 is list built from puzzlePiece entries { "Piece1", "Piece2"}
+            currSolvedList.Add(addPiece);
+            ResolvePuzzle();
+        }
+        public void ResolvePuzzle()
+        {
+
+            if (currSolvedList.Count == solutionList.Count)
+            {
+                if (isSequential)
+                {
+                    ListComparer();
+                    if (_sequenceGood)
+                    {
+                        _puzzleSolved = true;
+                    }
+                    else
+                    {
+                        _puzzleSolved = false;
+                    }
+                }
+                else
+                {
+                    ListChecker();
+                    if (_allPiecesCheck)
+                    {
+                        _puzzleSolved = true;
+                    }
+                    else
+                    {
+                        _puzzleSolved = false;
+                    }
+                }
+            }
+        }
+
+        // May need StorePuzzleState() TBD if information will be passed regarding state at savepoint
+
     }
 
-    // May need StorePuzzleState() TBD if information will be passed regarding state at savepoint
-     
+
+
 }
-
-
--- Assets/Scripts/HiddenWorld/Puzzle/PuzzlePiece.cs
+++ Assets/Scripts/HiddenWorld/Puzzle/PuzzlePiece.cs
@@ -1,87 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
-using Helpers;
+using HiddenWorld.Helpers;
 using Unity.VisualScripting;
 using UnityEngine;
 
-
-public class PuzzlePiece : Puzzle
+namespace HiddenWorld.Puzzle
 {
-    [SerializeField] private float _startPosX;
-    [SerializeField] private float _startPosY;
-    [SerializeField] private float _startPosZ;
-    [SerializeField] private float _startRotX;
-    [SerializeField] private float _startRotY;
-    [SerializeField] private float _startRotZ;
-    [SerializeField] private float _startScale;
-    [SerializeField] private float _endPosX;
-    [SerializeField] private float _endPosY;
-    [SerializeField] private float _endPosZ;
-    [SerializeField] private float _endRotX;
-    [SerializeField] private float _endRotY;
-    [SerializeField] private float _endRotZ;
-    [SerializeField] private float _endScale;
-
-    // how to implement initial and correct position when building puzzle:
-    // during dev move piece to starting position and store transform pos,rot,scale in private variables
-    // during dev move piece to correct end position and store transform pos,rot,scale in private variables
-
-    public bool isSet = false;
-    public bool isActive = false; // keep track of when a puzzle piece is being interacted with
-
-    // inherits from IInteractable
-    // enum PuzzleType (Jigsaw, Sequential, etc)
-
-    private void OnInteract()
+    public class PuzzlePiece : MonoBehaviour, IInteractable
     {
-        if (isActive) 
-        { isActive = false; 
-        }
-        else
+        public bool isSet = false;
+        public bool isActive = false; // keep track of when a puzzle piece is being interacted with
+        private Puzzle _puzzle;
+
+
+        // enum PuzzleType (Jigsaw, Sequential, etc)
+
+        public void SetPuzzleRef(Puzzle puzzle)
         {
-            isActive = true;
+            _puzzle = puzzle;
         }
-        
-        //determine if the piece is set correctly or not and if it is set correctly calls AddList2Piece() in Puzzle class
-        if (isSet)
+        public virtual void OnInteract()
         {
-        Puzzle:AddList2Piece("Piece1");
-        }
-    }
+            if (isActive)
+            {
+                isActive = false;
+            }
+            else
+            {
+                isActive = true;
+            }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        IInteractable interactable = other.GetComponent<IInteractable>();
-        if (interactable != null && other.CompareTag("PuzzlePiece"))
+            //determine if the piece is set correctly or not and if it is set correctly calls AddList2Piece() in Puzzle class
+            if (isSet)
+            {
+                _puzzle.AddList2Piece(gameObject.name);
+            }
+        }
+
+
+
+        // inherits from ISpyglassable
+        // OnSpyglassed() to display augmented/different sprite/graphic/etc.
+
+        // Start is called before the first frame update
+        void Start()
         {
-            // interactable.OnInteract(puzzle.interact); // need to determine what gets passed
+
         }
-    }
 
-    // depending on PuzzleType will have different interactions and will call the appropriate method(s) (puzzle piece will be moveable and rotatable)
-    // Rotate: rotates the PuzzlePiece
-    //    public void RotateTo(Quaternion newRotation)
-    //{
-    //    transform.rotation = newRotation;
-    //}
+        // Update is called once per frame
+        void Update()
+        {
 
-    //public void PuzzleReset() // may end up being (or calling) a puzzle sub-class dependent reset procedure
-    //{
-
-    //}
-
-    // inherits from ISpyglassable
-    // OnSpyglassed() to display augmented/different sprite/graphic/etc.
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
+
